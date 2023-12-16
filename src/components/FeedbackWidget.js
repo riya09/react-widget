81a9d69c551b8f6d9156b2b0e8c5ba4451139d29
riya09 <faria.ayir@gmail.com>
2023-12-17 01:10:26 +0600

    feat: create state for controlling rating

--- src/components/FeedbackWidget.js
+++ src/components/FeedbackWidget.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './widget.scss'
 
 const FeedbackWidget = (props) => {
@@ -6,12 +6,18 @@ const FeedbackWidget = (props) => {
     titleText = 'Rate your experience',
     buttonText = 'Submit',
     themeColor = '#3457dc',
-    handleSubmit
-  } = props
+    handleSubmit = () => {},
+  } = props;
+
+  const [rating, setRating] = useState(null);
 
   useEffect(() => {
     document.documentElement.style.setProperty('--base', themeColor);
-  }, [themeColor])
+  }, [themeColor]);
+
+  const submitRating = () => {
+    handleSubmit(rating)
+  }
 
   return (
     <div className="brand__feedbackWidget">
@@ -21,22 +27,23 @@ const FeedbackWidget = (props) => {
         </h1>
         <div className="feedbackWidget__rating">
           {[...Array(5)].map((_, k) => (
-            <label htmlFor={k+1} key={k}>
-              <input id={k+1} type="radio" name="rating" value={k+1} />
+            <label htmlFor={k + 1} key={k}>
+              <input onChange={() => setRating(k + 1)} id={k + 1} type="radio" name="rating" value={k + 1} />
               <span>{k + 1}</span>
             </label>
           ))}
         </div>
         <div className="feedbackWidget__btn--container">
           <button
-          className="feedbackWidget__btn--submit"
-          onClick={handleSubmit}
-        >
-          { buttonText }
-        </button>
+            className="feedbackWidget__btn--submit"
+            onClick={submitRating}
+            disabled={!rating}
+          >
+            {buttonText}
+          </button>
         </div>
       </div>
     </div>
   );
-}
-export default FeedbackWidget
+};
+export default FeedbackWidget;

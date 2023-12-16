62d2b8cbb2dd68c2fc3e347e4cde64591c65e067
riya09 <faria.ayir@gmail.com>
2023-12-17 02:20:02 +0600

    feat: add close and animation for toggling widget

--- src/components/FeedbackWidget.js
+++ src/components/FeedbackWidget.js
@@ -10,25 +10,49 @@ const FeedbackWidget = (props) => {
   } = props;
 
   const [rating, setRating] = useState(null);
+  const [hideWidget, toggleVisibility] = useState(true)
 
   useEffect(() => {
     document.documentElement.style.setProperty('--base', themeColor);
   }, [themeColor]);
 
+  useEffect(() => {
+    toggleVisibility(false)
+  }, [])
+
   const submitRating = () => {
     handleSubmit(rating)
+    toggleVisibility(true)
   }
 
   return (
     <div className="brand__feedbackWidget">
-      <div className="feedbackWidget__container">
-        <h4 className="feedbackWidget__title">
-          {titleText}
-        </h4>
+      <div className={`feedbackWidget__container${hideWidget ? " hidden" : ""}`}>
+        <div className="feedbackWidget__header">
+          <h4 className="feedbackWidget__title">{titleText}</h4>
+          <span onClick={() => toggleVisibility(true)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </span>
+        </div>
         <div className="feedbackWidget__rating">
           {[...Array(5)].map((_, k) => (
             <label htmlFor={k + 1} key={k}>
-              <input onChange={() => setRating(k + 1)} id={k + 1} type="radio" name="rating" value={k + 1} />
+              <input
+                onChange={() => setRating(k + 1)}
+                id={k + 1}
+                type="radio"
+                name="rating"
+                value={k + 1}
+              />
               <span>{k + 1}</span>
             </label>
           ))}
